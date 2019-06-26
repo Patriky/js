69db22ba90645793313e25b43adb5483cc11af66
Patriky <patriky@alunos.utfpr.edu.br>
2019-06-26 14:57:24 -0300

    fisrt test in js

---
+++ aula1/exercicioTaboada/calcula.js
@@ -0,0 +1,15 @@
+function taboada() {
+    var num = document.getElementById("num");
+    var tab = document.getElementById("taboada");
+    if (num.value.length == 0) {
+        alert("Campo vazio!")
+    } else {
+        var n = Number(num.value)
+        console.log(n)
+    }
+
+}
+
+function multiplica(params) {
+
+}
