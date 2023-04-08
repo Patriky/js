35b6413b219532eacd04adc0b222afc605519236
Patriky <patriky@alunos.utfpr.edu.br>
2023-04-08 16:22:40 -0300

    add new fields

--- exercícios/exercicioTaboada/calcula.js
+++ exercícios/exercicioTaboada/calcula.js
@@ -7,7 +7,7 @@ function taboada() {
         let n = Number(num.value);
         tab.innerText = ""
         for (let index = 1; index <= 10; index++) {
-            let item = document.createElement('option'); /* Cria um novo elemento html do tipo otion */
+            let item = document.createElement('option'); /* Cria um novo elemento html do tipo option */
             item.text = `${index} x ${n} = ${index*n}`; /* adiciona um novo text no item */
             item.value = `tab${index}`; /* adiciona um novo id para o item. Cada linha terá um id diferente */
             tab.appendChild(item) /*adiciona  um elemento no "item" */
